cffd5c0cdcb8b7a67a30721e9205bea002fb9a9c
mimimimizuki <mizuki0327.5@gmail.com>
2021-07-02 14:49:08 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module main
+
+go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
+	github.com/line/line-bot-sdk-go/v7 v7.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	err0 := godotenv.Load(fmt.Sprintf("%s.env", os.Getenv("GO_ENV")))
+	if err0 != nil {
+		fmt.Println("could not load env file")
+	}
+	// LINE Botクライアント生成する
+	// BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
+	bot, err := linebot.New(
+		os.Getenv("SECRET"),
+		os.Getenv("CHANNEL_ACCESS_TOKEN"),
+	)
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// テキストメッセージを生成する
+	message := linebot.NewTextMessage("hello, world")
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("send")
+}
