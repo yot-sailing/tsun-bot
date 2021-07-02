85e20d4d76bf70cddc62f0b5980acacc272f5439
mimimimizuki <mizuki0327.5@gmail.com>
2021-07-02 21:29:20 +0900

    second commit

--- go.mod
+++ go.mod
@@ -3,7 +3,17 @@ module main
 go 1.16
 
 require (
+	github.com/Bowery/prompt v0.0.0-20190916142128-fa8279994f75 // indirect
+	github.com/dchest/safefile v0.0.0-20151022103144-855e8d98f185 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/kardianos/govendor v1.0.9 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
 	github.com/line/line-bot-sdk-go/v7 v7.10.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/tools/godep v0.0.0-20180126220526-ce0bfadeb516 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
+
+// type rawFlexContainer struct {
+// 	Type      FlexContainerType `json:"type"`
+// 	Container FlexContainer     `json:"-"`
+// }
 
 func main() {
 	err0 := godotenv.Load(fmt.Sprintf("%s.env", os.Getenv("GO_ENV")))
@@ -31,4 +37,218 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("send")
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		events, err := bot.ParseRequest(req)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
+		}
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				case *linebot.StickerMessage:
+					replyMessage := fmt.Sprintf(
+						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+	// This is just sample code.
+	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+
+	// jsonData := []byte(`
+	// {
+	// 	"type": "bubble",
+	// 	"hero": {
+	// 	  "type": "box",
+	// 	  "layout": "vertical",
+	// 	  "contents": [
+	// 		{
+	// 		  "type": "text",
+	// 		  "text": "site",
+	// 		  "size": "xl",
+	// 		  "color": "#ffffff",
+	// 		  "align": "center"
+	// 		}
+	// 	  ],
+	// 	  "backgroundColor": "#666666"
+	// 	},
+	// 	"body": {
+	// 	  "type": "box",
+	// 	  "layout": "vertical",
+	// 	  "contents": [
+	// 		{
+	// 		  "type": "text",
+	// 		  "text": "tsuntsunでサイトを積み始めたら爆速で消化できるようになった話",
+	// 		  "weight": "bold",
+	// 		  "size": "xl",
+	// 		  "wrap": true
+	// 		},
+	// 		{
+	// 		  "type": "box",
+	// 		  "layout": "vertical",
+	// 		  "margin": "lg",
+	// 		  "spacing": "sm",
+	// 		  "contents": [
+	// 			{
+	// 			  "type": "box",
+	// 			  "layout": "baseline",
+	// 			  "spacing": "sm",
+	// 			  "contents": [
+	// 				{
+	// 				  "type": "text",
+	// 				  "text": "URL",
+	// 				  "color": "#aaaaaa",
+	// 				  "size": "sm",
+	// 				  "flex": 2
+	// 				},
+	// 				{
+	// 				  "type": "text",
+	// 				  "text": "http://localhost:8080",
+	// 				  "wrap": true,
+	// 				  "color": "#666666",
+	// 				  "size": "sm",
+	// 				  "flex": 5
+	// 				}
+	// 			  ]
+	// 			},
+	// 			{
+	// 			  "type": "box",
+	// 			  "layout": "baseline",
+	// 			  "spacing": "sm",
+	// 			  "contents": [
+	// 				{
+	// 				  "type": "text",
+	// 				  "text": "created",
+	// 				  "color": "#aaaaaa",
+	// 				  "size": "sm",
+	// 				  "flex": 2,
+	// 				  "wrap": true
+	// 				},
+	// 				{
+	// 				  "type": "text",
+	// 				  "text": "2021/07/02",
+	// 				  "wrap": true,
+	// 				  "color": "#666666",
+	// 				  "size": "sm",
+	// 				  "flex": 5
+	// 				}
+	// 			  ]
+	// 			},
+	// 			{
+	// 			  "type": "box",
+	// 			  "layout": "baseline",
+	// 			  "spacing": "sm",
+	// 			  "contents": [
+	// 				{
+	// 				  "type": "text",
+	// 				  "text": "total time",
+	// 				  "color": "#aaaaaa",
+	// 				  "size": "sm",
+	// 				  "flex": 2,
+	// 				  "wrap": true
+	// 				},
+	// 				{
+	// 				  "type": "text",
+	// 				  "text": "5min",
+	// 				  "wrap": true,
+	// 				  "color": "#666666",
+	// 				  "size": "sm",
+	// 				  "flex": 5
+	// 				}
+	// 			  ]
+	// 			}
+	// 		  ]
+	// 		}
+	// 	  ]
+	// 	},
+	// 	"footer": {
+	// 	  "type": "box",
+	// 	  "layout": "vertical",
+	// 	  "spacing": "sm",
+	// 	  "contents": [
+	// 		{
+	// 		  "type": "button",
+	// 		  "style": "link",
+	// 		  "height": "sm",
+	// 		  "action": {
+	// 			"type": "uri",
+	// 			"label": "read now",
+	// 			"uri": "https://linecorp.com"
+	// 		  }
+	// 		},
+	// 		{
+	// 		  "type": "button",
+	// 		  "style": "link",
+	// 		  "height": "sm",
+	// 		  "action": {
+	// 			"type": "uri",
+	// 			"label": "already read",
+	// 			"uri": "https://linecorp.com"
+	// 		  }
+	// 		},
+	// 		{
+	// 		  "type": "spacer",
+	// 		  "size": "sm"
+	// 		}
+	// 	  ],
+	// 	  "flex": 0
+	// 	}
+	//   }
+	// `)
+	// container, err := linebot.UnmarshalFlexMessageJSON(jsonData)
+	// if err != nil {
+	// 	// 正しくUnmarshalできないinvalidなJSONであればerrが返る
+	// 	fmt.Println("could not form json data")
+	// }
+	// message1 := linebot.NewFlexMessage("alt text", container)
 }
+
+// func UnmarshalFlexMessageJSON(data []byte) (FlexContainer, error) {
+// 	raw := rawFlexContainer{}
+// 	if err := json.Unmarshal(data, &raw); err != nil {
+// 		return nil, err
+// 	}
+// 	return raw.Container, nil
+// }
+
+// func (c *rawFlexContainer) UnmarshalJSON(data []byte) error {
+// 	type alias rawFlexContainer
+// 	raw := alias{}
+// 	if err := json.Unmarshal(data, &raw); err != nil {
+// 		return err
+// 	}
+// 	var container FlexContainer
+// 	switch raw.Type {
+// 	case FlexContainerTypeBubble:
+// 		container = &BubbleContainer{}
+// 	case FlexContainerTypeCarousel:
+// 		container = &CarouselContainer{}
+// 	default:
+// 		return errors.New("invalid container type")
+// 	}
+// 	if err := json.Unmarshal(data, container); err != nil {
+// 		return err
+// 	}
+// 	c.Type = raw.Type
+// 	c.Container = container
+// 	return nil
+// }
