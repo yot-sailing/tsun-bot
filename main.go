3b25c036e98f9b208f857509b75b232e11aa6b48
mimimimizuki <mizuki0327.5@gmail.com>
2021-07-02 22:04:45 +0900

    check flex message

--- main.go
+++ main.go
@@ -30,13 +30,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// テキストメッセージを生成する
-	message := linebot.NewTextMessage("hello, world")
-	// テキストメッセージを友達登録しているユーザー全員に配信する
-	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("send")
 	// Setup HTTP Server for receiving requests from LINE platform
 	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
 		events, err := bot.ParseRequest(req)
@@ -54,6 +47,134 @@ func main() {
 				case *linebot.TextMessage:
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
 						log.Print(err)
+					}
+					jsonData := []byte(`
+					{
+						"body": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "contents": [
+							{
+							  "type": "box",
+							  "layout": "vertical",
+							  "margin": "lg",
+							  "spacing": "sm",
+							  "contents": [
+								{
+								  "type": "box",
+								  "layout": "baseline",
+								  "spacing": "sm",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "URL",
+									  "color": "#aaaaaa",
+									  "size": "sm",
+									  "flex": 2
+									},
+									{
+									  "type": "text",
+									  "text": "http://localhost:8080",
+									  "wrap": true,
+									  "color": "#666666",
+									  "size": "sm",
+									  "flex": 5
+									}
+								  ]
+								},
+								{
+								  "type": "box",
+								  "layout": "baseline",
+								  "spacing": "sm",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "created",
+									  "color": "#aaaaaa",
+									  "size": "sm",
+									  "flex": 2,
+									  "wrap": true
+									},
+									{
+									  "type": "text",
+									  "text": "2021/07/02",
+									  "wrap": true,
+									  "color": "#666666",
+									  "size": "sm",
+									  "flex": 5
+									}
+								  ]
+								},
+								{
+								  "type": "box",
+								  "layout": "baseline",
+								  "spacing": "sm",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "total time",
+									  "color": "#aaaaaa",
+									  "size": "sm",
+									  "flex": 2,
+									  "wrap": true
+									},
+									{
+									  "type": "text",
+									  "text": "5min",
+									  "wrap": true,
+									  "color": "#666666",
+									  "size": "sm",
+									  "flex": 5
+									}
+								  ]
+								}
+							  ]
+							}
+						  ]
+						},
+						"footer": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "spacing": "sm",
+						  "contents": [
+							{
+							  "type": "button",
+							  "style": "link",
+							  "height": "sm",
+							  "action": {
+								"type": "uri",
+								"label": "read now",
+								"uri": "https://linecorp.com"
+							  }
+							},
+							{
+							  "type": "button",
+							  "style": "link",
+							  "height": "sm",
+							  "action": {
+								"type": "uri",
+								"label": "already read",
+								"uri": "https://linecorp.com"
+							  }
+							},
+							{
+							  "type": "spacer",
+							  "size": "sm"
+							}
+						  ],
+						  "flex": 0
+						}
+					  }
+					`)
+					container, err_f := linebot.UnmarshalFlexMessageJSON(jsonData)
+					if err_f != nil {
+						fmt.Println("could not read json data")
+					}
+					if _, err4 := bot.ReplyMessage(
+						event.ReplyToken,
+						linebot.NewFlexMessage("alt text", container),
+					).Do(); err4 != nil {
+						fmt.Println(err4)
 					}
 				case *linebot.StickerMessage:
 					replyMessage := fmt.Sprintf(
@@ -74,146 +195,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// jsonData := []byte(`
-	// {
-	// 	"type": "bubble",
-	// 	"hero": {
-	// 	  "type": "box",
-	// 	  "layout": "vertical",
-	// 	  "contents": [
-	// 		{
-	// 		  "type": "text",
-	// 		  "text": "site",
-	// 		  "size": "xl",
-	// 		  "color": "#ffffff",
-	// 		  "align": "center"
-	// 		}
-	// 	  ],
-	// 	  "backgroundColor": "#666666"
-	// 	},
-	// 	"body": {
-	// 	  "type": "box",
-	// 	  "layout": "vertical",
-	// 	  "contents": [
-	// 		{
-	// 		  "type": "text",
-	// 		  "text": "tsuntsunでサイトを積み始めたら爆速で消化できるようになった話",
-	// 		  "weight": "bold",
-	// 		  "size": "xl",
-	// 		  "wrap": true
-	// 		},
-	// 		{
-	// 		  "type": "box",
-	// 		  "layout": "vertical",
-	// 		  "margin": "lg",
-	// 		  "spacing": "sm",
-	// 		  "contents": [
-	// 			{
-	// 			  "type": "box",
-	// 			  "layout": "baseline",
-	// 			  "spacing": "sm",
-	// 			  "contents": [
-	// 				{
-	// 				  "type": "text",
-	// 				  "text": "URL",
-	// 				  "color": "#aaaaaa",
-	// 				  "size": "sm",
-	// 				  "flex": 2
-	// 				},
-	// 				{
-	// 				  "type": "text",
-	// 				  "text": "http://localhost:8080",
-	// 				  "wrap": true,
-	// 				  "color": "#666666",
-	// 				  "size": "sm",
-	// 				  "flex": 5
-	// 				}
-	// 			  ]
-	// 			},
-	// 			{
-	// 			  "type": "box",
-	// 			  "layout": "baseline",
-	// 			  "spacing": "sm",
-	// 			  "contents": [
-	// 				{
-	// 				  "type": "text",
-	// 				  "text": "created",
-	// 				  "color": "#aaaaaa",
-	// 				  "size": "sm",
-	// 				  "flex": 2,
-	// 				  "wrap": true
-	// 				},
-	// 				{
-	// 				  "type": "text",
-	// 				  "text": "2021/07/02",
-	// 				  "wrap": true,
-	// 				  "color": "#666666",
-	// 				  "size": "sm",
-	// 				  "flex": 5
-	// 				}
-	// 			  ]
-	// 			},
-	// 			{
-	// 			  "type": "box",
-	// 			  "layout": "baseline",
-	// 			  "spacing": "sm",
-	// 			  "contents": [
-	// 				{
-	// 				  "type": "text",
-	// 				  "text": "total time",
-	// 				  "color": "#aaaaaa",
-	// 				  "size": "sm",
-	// 				  "flex": 2,
-	// 				  "wrap": true
-	// 				},
-	// 				{
-	// 				  "type": "text",
-	// 				  "text": "5min",
-	// 				  "wrap": true,
-	// 				  "color": "#666666",
-	// 				  "size": "sm",
-	// 				  "flex": 5
-	// 				}
-	// 			  ]
-	// 			}
-	// 		  ]
-	// 		}
-	// 	  ]
-	// 	},
-	// 	"footer": {
-	// 	  "type": "box",
-	// 	  "layout": "vertical",
-	// 	  "spacing": "sm",
-	// 	  "contents": [
-	// 		{
-	// 		  "type": "button",
-	// 		  "style": "link",
-	// 		  "height": "sm",
-	// 		  "action": {
-	// 			"type": "uri",
-	// 			"label": "read now",
-	// 			"uri": "https://linecorp.com"
-	// 		  }
-	// 		},
-	// 		{
-	// 		  "type": "button",
-	// 		  "style": "link",
-	// 		  "height": "sm",
-	// 		  "action": {
-	// 			"type": "uri",
-	// 			"label": "already read",
-	// 			"uri": "https://linecorp.com"
-	// 		  }
-	// 		},
-	// 		{
-	// 		  "type": "spacer",
-	// 		  "size": "sm"
-	// 		}
-	// 	  ],
-	// 	  "flex": 0
-	// 	}
-	//   }
-	// `)
 	// container, err := linebot.UnmarshalFlexMessageJSON(jsonData)
 	// if err != nil {
 	// 	// 正しくUnmarshalできないinvalidなJSONであればerrが返る
